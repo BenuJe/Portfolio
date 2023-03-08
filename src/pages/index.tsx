3d3c345ad894e1fec6535012882f0cf3f9c5e1b4
Benoit JEANNOT <benoit.jeannot@hotmail.fr>
2023-03-08 22:51:08 +0100

    correction des bugs, ajouts des images et animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
-	AiFillTwitterCircle,
 	AiFillLinkedin,
 	AiFillGithub,
 } from "react-icons/ai";
 import Image from "next/image";
 import Benoit from "../../public/BenoitJ.png";
-import design from "../../public/design.png";
-import code from "../../public/code.png";
-import consulting from "../../public/consulting.png";
-import web1 from "../../public/web1.png";
-import web2 from "../../public/web2.png";
-import web3 from "../../public/web3.png";
-import web4 from "../../public/web4.png";
-import web5 from "../../public/web5.png";
-import web6 from "../../public/web6.png";
+import p2 from "../../public/p2.png";
+import p3 from "../../public/p3.png";
+import p4 from "../../public/p4.png";
+import p5 from "../../public/p5.png";
+import p6 from "../../public/p6.png";
+import p7 from "../../public/p7.png";
+import dossier from "../../public/folder.png";
+import code from "../../public/coding.png";
+
 import { useState } from "react";
 
 export default function Home() {
@@ -29,7 +28,7 @@ export default function Home() {
 					content="Porte feuille de compétence de Benoit JEANNOT"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 			<main className=" bg-slate-50 px-10 md:px-20 lg:px-40 dark:bg-slate-800">
 				{/* première page */}
@@ -59,7 +58,7 @@ export default function Home() {
 						</h2>
 						<h3 className="text-2xl py-2 md:text-3xl dark:text-yellow-100">Développeur Web</h3>
 						<p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
-							{`Bienvenu sur mon Portfolio ! Laissez moi me présenter.`}
+							{`Bienvenue sur mon Portfolio ! Laissez-moi me présenter.`}
 							<br></br>
 						  	{`Passionné d’informatique j’ai commencé mon cursus par un BTS SIO (Service
 						  	Informatique aux Organisation) et je l’ai terminé avec la formation Développeur Web
@@ -84,18 +83,18 @@ export default function Home() {
 
 					<div className="lg:flex gap-10 justify-around">
 
-						<div className="text-center shadow-md shadow-slate-800 p-10 rounded-xl my-10 dark:shadow-amber-100 dark:shadow-md">
+						<div className="text-center shadow-md p-10 shadow-slate-800 pb-10 rounded-xl my-10 dark:shadow-amber-100 dark:shadow-md">
 							<Image
-								className=" mx-auto "
-								src={code}
-								width={100}
-								height={100}
+								className=" mx-auto  animate-pulse hover:animate-none"
+								src={dossier}
+								width={200}
+								height={200}
 								alt="design"
 							/>
 							<h3 className="text-lg font-medium pt-8 pb-2 dark:text-cyan-300">
 								Environnement de travail
 							</h3>
-							<p className="text-md py-2 leading-8 text-gray-800 dark:text-yellow-100">
+							<p className="text-md leading-8 text-gray-800 dark:text-yellow-100 ">
 							{`Voici les logiciels que j'utilise pour mener à bien mes objectifs et réaliser mes projets`}
 							</p>
 							<h4 className="py-4 dark:text-cyan-300">Outils utilisés :</h4>
@@ -106,19 +105,19 @@ export default function Home() {
 							<p className="text-gray-800 py-1 dark:text-yellow-100  basis-1/2">Figma</p>
 							</div>
 						</div>
-						<div className="text-center  shadow-md shadow-slate-800 p-10 rounded-xl my-10 dark:shadow-amber-100 ">
+						<div className="text-center  shadow-md shadow-slate-800 p-10 pt rounded-xl my-10 dark:shadow-amber-100 ">
 							<Image
-								className=" mx-auto "
+								className=" mx-auto animate-pulse hover:animate-none"
 								src={code}
-								width={100}
-								height={100}
+								width={200}
+								height={200}
 								alt="design"
 							/>
 							<h3 className="text-lg font-medium pt-8 pb-2 dark:text-cyan-300">
 								Programmation
 							</h3>
-							<p className="text-md py-2 leading-8 text-gray-800 dark:text-yellow-100">
-								{`De la programmation ? Oui ! Mais avec quels languages et quel framework ?`}
+							<p className="text-md  leading-8 text-gray-800 dark:text-yellow-100">
+								{`De la programmation ? Oui ! Mais avec quels langages et quels frameworks ?`}
 							</p>
 							<h4 className="py-4 dark:text-cyan-300">Frameworks et languages :</h4>
 							<div className="flex flex-row flex-wrap">
@@ -142,30 +141,34 @@ export default function Home() {
           <div>
             <h3 className="text-5xl font-medium pt-8 pb-2 dark:text-cyan-300 text-center">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800  dark:text-yellow-100 text-center">
-								{`J'ai pu me présenter, vous présenter les logiciels que j'utilise, les frameworks ainsi que les languages que je maitrisais.`}
+								{`J'ai pu me présenter, vous montrer les logiciels que j'utilise, les frameworks ainsi que les languages que je maîtrisais.`}
 								<br></br>
-								{`Il ne me reste plus qu'a vous montrer ce que je sais faire ! Ca tombe bien la suite se trouve juste en dessous.`}
+								{`Il ne me reste plus qu'à vous montrer ce que je sais faire ! Ca tombe bien la suite se trouve juste en dessous.`}
 							</p>
           </div>
 
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
-              <Image src={web1} className=" rounded-lg object-cover"  layout="responsive" alt="1" />
+				<a href="https://github.com/BenuJe/p7">
+              <Image src={p7} className=" rounded-lg object-cover hover:contrast-75"  layout="responsive" alt="1" /> </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image src={web2} className=" rounded-lg object-cover"  layout="responsive" alt="1" />
+              <Image src={p6} className=" rounded-lg object-cover hover:contrast-75"  layout="responsive" alt="1" />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image src={web3} className=" rounded-lg object-cover" layout="responsive" alt="1" />
+              <Image src={p5} className=" rounded-lg object-cover hover:contrast-75" layout="responsive" alt="1" />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image src={web4} className=" rounded-lg object-cover" layout="responsive" alt="1" />
+				<a href="https://benuje.github.io/p4/">
+              <Image src={p4} className=" rounded-lg object-cover hover:contrast-75" layout="responsive" alt="1" /></a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image src={web5} className=" rounded-lg object-cover" layout="responsive" alt="1" />
+				<a href="https://benuje.github.io/p3/">
+              <Image src={p3} className=" rounded-lg object-cover hover:contrast-75" layout="responsive" alt="1" /></a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image src={web6} className=" rounded-lg object-cover" layout="responsive" alt="1" />
+				<a href="https://benuje.github.io/P2Booky/test.html">
+              <Image src={p2} className=" rounded-lg object-cover hover:contrast-75" layout="responsive" alt="1" /></a>
             </div>
           </div>
         </section>
